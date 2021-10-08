83088436c86da2766fa8d595984ac1f661f11d7b
JohnAsu <asucanchrist123@gmai.com>
2021-10-08 21:07:17 +0800

    first commit

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,27 @@
+
+@extends('base')
+
+@section('content')
+<style>
+  body{
+    background: url(background.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  }
+  .container{
+    text-align: center;
+    color: white;
+  }
+  .title{
+    font-size: 80px;
+    margin-top: 25px;
+    font-style: italic;
+  }
+</style>
+
+<div class="container">
+  <h1 class="title" >This is My Life!</h1>
+</div>
+
+@endsection 
+  
